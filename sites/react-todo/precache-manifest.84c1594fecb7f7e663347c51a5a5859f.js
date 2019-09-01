ef92266e084a683fbed89c4accde9b60dfd19e43
Sergey Voskresenskiy <work.sergey.voskeresnskiy@gmail.com>
2019-09-02 02:59:09 +0300

    add react-todo

---
+++ sites/react-todo/precache-manifest.84c1594fecb7f7e663347c51a5a5859f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e8925631d9f94823d7cbcb22d75cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81325c1f51284dbf693c",
+    "url": "/static/css/main.b3f07435.chunk.css"
+  },
+  {
+    "revision": "2c3198f1151909c976ee",
+    "url": "/static/js/2.bc0d02fe.chunk.js"
+  },
+  {
+    "revision": "81325c1f51284dbf693c",
+    "url": "/static/js/main.cd3db288.chunk.js"
+  },
+  {
+    "revision": "8277e3460bb72fd0789b",
+    "url": "/static/js/runtime~main.240aadcb.js"
+  }
+]);
